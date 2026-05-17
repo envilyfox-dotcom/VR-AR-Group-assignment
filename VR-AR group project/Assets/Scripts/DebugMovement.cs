6d32f007ddfcc6ac1bd80f70828bd3b83fbc4528
Darrenst17 <darrenst17@gmail.com>
2026-05-18 02:08:58 +0800

    DebugMovement SCript

--- VR-AR group project/Assets/Scripts/DebugMovement.cs
+++ VR-AR group project/Assets/Scripts/DebugMovement.cs
@@ -1,34 +1,34 @@
-using UnityEngine;
+    using UnityEngine;
 
-public class DebugMovement : MonoBehaviour
-{
-    private Vector3 lastXROrigin;
-    private Vector3 lastCameraOffset;
-    private Vector3 lastCamera;
-
-    void Start()
+    public class DebugMovement : MonoBehaviour
     {
-        lastXROrigin = transform.position;
-        lastCameraOffset = transform.Find("Camera Offset").position;
-        lastCamera = Camera.main.transform.position;
+        private Vector3 lastXROrigin;
+        private Vector3 lastCameraOffset;
+        private Vector3 lastCamera;
+
+        void Start()
+        {
+            lastXROrigin = transform.position;
+            lastCameraOffset = transform.Find("Camera Offset").position;
+            lastCamera = Camera.main.transform.position;
+        }
+
+        void Update()
+        {
+            Vector3 xrDelta = transform.position - lastXROrigin;
+            Vector3 camOffsetDelta = transform.Find("Camera Offset").position - lastCameraOffset;
+            Vector3 camDelta = Camera.main.transform.position - lastCamera;
+
+            xrDelta.y = 0;
+            camOffsetDelta.y = 0;
+            camDelta.y = 0;
+
+            Debug.Log($"XR Origin moved: {xrDelta.magnitude}");
+            Debug.Log($"Camera Offset moved: {camOffsetDelta.magnitude}");
+            Debug.Log($"Camera moved: {camDelta.magnitude}");
+
+            lastXROrigin = transform.position;
+            lastCameraOffset = transform.Find("Camera Offset").position;
+            lastCamera = Camera.main.transform.position;
+        }
     }
-
-    void Update()
-    {
-        Vector3 xrDelta = transform.position - lastXROrigin;
-        Vector3 camOffsetDelta = transform.Find("Camera Offset").position - lastCameraOffset;
-        Vector3 camDelta = Camera.main.transform.position - lastCamera;
-
-        xrDelta.y = 0;
-        camOffsetDelta.y = 0;
-        camDelta.y = 0;
-
-        Debug.Log($"XR Origin moved: {xrDelta.magnitude}");
-        Debug.Log($"Camera Offset moved: {camOffsetDelta.magnitude}");
-        Debug.Log($"Camera moved: {camDelta.magnitude}");
-
-        lastXROrigin = transform.position;
-        lastCameraOffset = transform.Find("Camera Offset").position;
-        lastCamera = Camera.main.transform.position;
-    }
-}
