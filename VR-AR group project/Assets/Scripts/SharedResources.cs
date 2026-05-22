18cb2a1c8eadbbe402fc7d9434fae5346530ab4b
envilyfox-dotcom <envilyfox@gmail.com>
2026-05-22 14:33:32 +0800

    Hui's room to trophy

--- VR-AR group project/Assets/Scripts/SharedResources.cs
+++ VR-AR group project/Assets/Scripts/SharedResources.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 
 public static class SharedResources
 {
-    public static int[] levelIndices = { 1, 2, 3 };
+    public static int[] levelIndices = { 1, 2, 3, 4 };
     public static int currentLevel = 0; // managed only by PortalTrigger
 }
